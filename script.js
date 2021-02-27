d550ced3c40a0caceda69ab6f21a81023b1e355a
Olu Olageshin <44141085+lagesh@users.noreply.github.com>
2021-02-27 22:34:52 +0000

    refactor everything

--- script.js
+++ script.js
@@ -1,32 +1,7 @@
-
-/*
 $(document).ready(() => {
   let $chevron = $('.icon');
   let $answer = $('.answer');
+  // let $downicon = $('.down');
+  // let $upicon = $('.up');
 
-  $chevron.on('click', (event) => {
-    $answer.slideToggle();
-  })
-})
-*/
-
-/*let icons = document.querySelectorAll('.icon');
-let answers = document.querySelectorAll('.answer');
-*/
-
-
-let icon = document.getElementsByClassName("icon");
-let i;
-
-for (i = 0; i < icon.length; i++) {
-  icon[i].addEventListener("click", function () {
-
-    /* Toggle between hiding and showing the active panel */
-    var answer = this.nextElementSibling;
-    if (answer.style.display === "block") {
-      answer.style.display = "none";
-    } else {
-      answer.style.display = "block";
-    }
-  });
-}
+ 
