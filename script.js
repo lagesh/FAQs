c303597328125546b75fff009c81626ce011b5a0
Olu Olageshin <44141085+lagesh@users.noreply.github.com>
2021-02-27 20:00:48 +0000

    js trial 1

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+/*
 $(document).ready(() => {
   let $chevron = $('.icon');
   let $answer = $('.answer');
@@ -6,3 +8,25 @@ $(document).ready(() => {
     $answer.slideToggle();
   })
 })
+*/
+
+/*let icons = document.querySelectorAll('.icon');
+let answers = document.querySelectorAll('.answer');
+*/
+
+
+let icon = document.getElementsByClassName("icon");
+let i;
+
+for (i = 0; i < icon.length; i++) {
+  icon[i].addEventListener("click", function () {
+
+    /* Toggle between hiding and showing the active panel */
+    var answer = this.nextElementSibling;
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
+    } else {
+      answer.style.display = "block";
+    }
+  });
+}
