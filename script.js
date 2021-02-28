61e397be9dcd9407b7638499f4d1d4072fb7e139
Olu Olageshin <44141085+lagesh@users.noreply.github.com>
2021-02-28 01:02:29 +0000

    one collapsible faq

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const accordionItemHeaders = document.querySelectorAll('.accordion-item-header')
 
 accordionItemHeaders.forEach(accordionItemHeader => {
   accordionItemHeader.addEventListener('click', event => {
+    const currentlyActiveHeader = document.querySelector('.accordion-item-header.active');
+    if(currentlyActiveHeader && currentlyActiveHeader !== accordionItemHeader) {
+      currentlyActiveHeader.classList.toggle('active');
+      currentlyActiveHeader.nextElementSibling.style.maxHeight = 0;
+    }
     accordionItemHeader.classList.toggle('active');
     const accordionItemBody = accordionItemHeader.nextElementSibling;
     if (accordionItemHeader.classList.contains('active')) {
